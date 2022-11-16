fe20d32282b31e6a0ac614f67de2278f4d4ae63e
Merradou-Abderrahmane <92019711+Merradou-Abderrahmane@users.noreply.github.com>
2022-11-16 22:53:13 +0100

    array methods ( error )

---
+++ ES6/Array Methods/script.js
@@ -0,0 +1,11 @@
+// import react and reactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+  ReactDOM.render(<Hello />, document.getElementById('root'))
+
+
+const array = ["banana", "apple", "orange"];
+const list = array.map((item,)=> { <p> {item} </p> });
+ReactDOM.render(list, document.getElementById('root'));
+
